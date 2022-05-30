687e3da3a0f92bf834fe5b8d1a1bbb602cd82991
SmallBear15 <106562190+SmallBear15@users.noreply.github.com>
2022-05-30 19:43:20 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+box = createSprite (40,30,50,50)
+}
+
+function draw() 
+{
+  background(30);
+drawSprites();
+if (keyDown(UP_ARROW)) {
+  box.y = box.y -2;
+}
+if (keyDown(DOWN_ARROW)){
+box.y = box.y +2;
+}
+if (keyDown(RIGHT_ARROW)) {
+box.x = box.x +2;
+}
+if (keyDown(LEFT_ARROW)){
+box.x = box.x -2;
+}
+}
+
+
+
+
